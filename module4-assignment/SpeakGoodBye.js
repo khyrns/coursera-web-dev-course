99e301239cb1bbafd16f22999e064007d7ec447c
kheyransa <kheyransa.aliyeva@gmail.com>
2021-02-14 03:17:48 +0400

    assignment4

---
+++ module4-assignment/SpeakGoodBye.js
@@ -0,0 +1,15 @@
+
+(function (window) {
+	
+	var byeSpeaker = {};
+	byeSpeaker.speak = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	var speakWord = "Good Bye";
+
+	window.byeSpeaker = byeSpeaker;
+
+})(window);
+
+
